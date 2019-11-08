04cfe4bcbb76813fdb4ba7ba5642e94f3acddda8
camysandhu <fmsharmn@gmail.com>
2019-11-08 17:51:42 -0500

    view detail func

--- C0764266_MidTerm_MAD3115F2019/CustomerDetailsViewController.swift
+++ C0764266_MidTerm_MAD3115F2019/CustomerDetailsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CustomerDetailsViewController: UIViewController {
+class CustomerDetailsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     var custDetails:Customer?=nil
     @IBOutlet weak var lblOne: UILabel!
     @IBOutlet weak var lblTwo: UILabel!
@@ -17,10 +17,48 @@ class CustomerDetailsViewController: UIViewController {
         super.viewDidLoad()
         lblOne.text = custDetails?.customerEmail
         lblTwo.text = custDetails?.fullName
-       
+       self.billTable.delegate=self
+              self.billTable.dataSource=self
+        let rButton = UIBarButtonItem(title: "Add Bill", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.toAddBill))
+        self.navigationItem.rightBarButtonItem = rButton
     }
     
     
+    let addBillBtn = UIBarButtonItem()
+      
+      @objc func toAddBill(){
+          self.performSegue(withIdentifier: "addingBillVC", sender: nil)
+      }
+      
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return ((customerdetails?.billDictionary.count)!)
+   }
+   
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "billCell")!
+       let currentBill = customerdetails!.billDictionary[indexPath.row + 1]
+   var detail = ""
+       if currentBill?.billType == billTypes.Mobile{
+           cell.textLabel?.numberOfLines = 12
+           let mobileBill = currentBill as! Mobile
+         detail = "\nBill Type : Mobile \nManufacturer Name : \(mobileBill.mobileManufacturer) \nPlan Name : \(mobileBill.planName) \nMobile Number : \(mobileBill.mobileNumber) \nInternet used : \(mobileBill.internetUsed.data()) \nMinutes Used : \(mobileBill.minuteUsed.minutes()) "
+       } else {
+           if currentBill?.billType == billTypes.Hydro{
+               cell.textLabel?.numberOfLines = 12
+               let hydroBill = currentBill as! Hydro
+               detail = "\nBill Type : Hydro \nAgency Name : \(hydroBill.agencyName) \nUnit Consumed : \(hydroBill.unitconsumed.unit())"
+           } else {
+               if currentBill?.billType == billTypes.Internet{
+                   cell.textLabel?.numberOfLines = 12
+                   let internetBill = currentBill as! Internet
+                   detail = "\nBill Type : Internet \nProvider Name : \(internetBill.providerName) \nInternet Used : \(internetBill.internetUsed.data())"
+               }
+           }
+       }
+       cell.textLabel?.text = "Bill ID : \(String(describing: currentBill!.Id)) \nBill Date : \(String(describing: currentBill!.billDate.getForamttedDate())) \nBill Total : \(String(describing: currentBill!.totalBillAmount.currency())) \(detail)"
+       return cell
+   }
+
 
    
 
--- C0764266_MidTerm_MAD3115F2019/Singleton.swift
+++ C0764266_MidTerm_MAD3115F2019/Singleton.swift
@@ -63,46 +63,52 @@ class Singleton: NSObject
 
     func createCust()
     {
-        
+            let b1 = Mobile(billId: 1, billDate: Date(), billType: billTypes.Mobile, totalBillAmount: 42.5, mobileManufacturerName: "Apple", planName: "BigData", mobileNumber: 647123432, internetGbUsed: 23, minuteUsed: 12)
+               
+               
+               let b2 = Mobile(billId: 2, billDate: Date(), billType: billTypes.Mobile, totalBillAmount: 65.4, mobileManufacturerName: "Samsung", planName: "BigData and Calls", mobileNumber: 9471234545, internetGbUsed: 23, minuteUsed: 12)
+               
+               
+               
+               let b3 = Internet(billId: 11, billDate: Date(), billType: billTypes.Internet, totalBillAmount: 176.8, providerName: "Bell", internetGbUsed: 32)
+               
+               
+               let b4 = Internet(billId: 2, billDate: Date(), billType: billTypes.Internet, totalBillAmount: 286.5, providerName: "Freedom", internetGbUsed: 49)
+               
+               
+               
+               let b5 = Hydro(billId: 1, billDate: Date(), billType: billTypes.Hydro, totalBillAmount: 122.8, agencyName: "Brampton Energy", unitConsumed: 56)
+               
+               
+               let b6 = Hydro(billId: 2, billDate: Date(), billType: billTypes.Hydro, totalBillAmount: 145.4, agencyName: "Toronto Lines", unitConsumed: 76)
         
         let c1 = Customer(customerID: 1, customerFName: "Camy", customerLName: "Sandhu", customerEmail: "fmscamy@gmail.com ")
         addingCustomer(customer: c1)
+        c1.addBillToCustomers(bill: b1)
+        c1.addBillToCustomers(bill: b6)
+        
         let c2 = Customer(customerID: 2, customerFName: "Charan", customerLName: "kaur", customerEmail: "ckaur@gmail.com ")
         addingCustomer(customer: c2)
+        c2.addBillToCustomers(bill: b3)
+        c2.addBillToCustomers(bill: b1)
         let c3 = Customer(customerID: 3, customerFName: "Komal", customerLName: "Kaur", customerEmail: "kkaur321@gmail.com ")
         addingCustomer(customer: c3)
+        c3.addBillToCustomers(bill: b4)
+        c3.addBillToCustomers(bill: b1)
+        c3.addBillToCustomers(bill: b6)
         let c4 = Customer(customerID: 4, customerFName: "Harchandbir", customerLName: "Singh", customerEmail: "hc@gmail.com ")
         addingCustomer(customer: c4)
-        let c5 = Customer(customerID: 5, customerFName: "Ankita", customerLName: "Sharma", customerEmail: "ankita@gmail.com ")
-        addingCustomer(customer: c5)
-        let c6 = Customer(customerID: 6, customerFName: "Megha", customerLName: "Arora", customerEmail: "arora@gmail.com ")
-        addingCustomer(customer: c6)
-        
-        let b1 = Mobile(billId: 1, billDate: Date(), billType: billTypes.Mobile, totalBillAmount: 42.5, mobileManufacturerName: "Apple", planName: "BigData", mobileNumber: 647123432, internetGbUsed: 23, minuteUsed: 12)
         
         
-        let b2 = Mobile(billId: 2, billDate: Date(), billType: billTypes.Mobile, totalBillAmount: 65.4, mobileManufacturerName: "Samsung", planName: "BigData and Calls", mobileNumber: 9471234545, internetGbUsed: 23, minuteUsed: 12)
+       c4.addBillToCustomers(bill: b1)
+              c4.addBillToCustomers(bill: b2)
         
-        
-        
-        let b3 = Internet(billId: 1, billDate: Date(), billType: billTypes.Internet, totalBillAmount: 176.8, providerName: "Bell", internetGbUsed: 32)
-        
-        
-        let b4 = Internet(billId: 2, billDate: Date(), billType: billTypes.Internet, totalBillAmount: 286.5, providerName: "Freedom", internetGbUsed: 49)
-        
-        
-        
-        let b5 = Hydro(billId: 1, billDate: Date(), billType: billTypes.Hydro, totalBillAmount: 122.8, agencyName: "Brampton Energy", unitConsumed: 56)
-        
-        
-        let b6 = Hydro(billId: 2, billDate: Date(), billType: billTypes.Hydro, totalBillAmount: 145.4, agencyName: "Toronto Lines", unitConsumed: 76)
-        
-        c1.addBillToCustomers(bill: b1)
-        c1.addBillToCustomers(bill: b2)
-        c1.addBillToCustomers(bill: b3)
-        c2.addBillToCustomers(bill: b4)
-        c3.addBillToCustomers(bill: b5)
-        c2.addBillToCustomers(bill: b6)
+//        
+//        c1.addBillToCustomers(bill: b2)
+//        c1.addBillToCustomers(bill: b3)
+//        c2.addBillToCustomers(bill: b4)
+//        c3.addBillToCustomers(bill: b5)
+//        c2.addBillToCustomers(bill: b6)
 
     }
 
